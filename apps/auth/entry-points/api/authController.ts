5985c7304946f2b405bf7a1b9a4b79884eccd267
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2025-03-08 00:53:20 +0500

    Enpoint: Added login endpoint

--- apps/auth/entry-points/api/authController.ts
+++ apps/auth/entry-points/api/authController.ts
@@ -1,24 +1,48 @@
 import express, { Request, Response, NextFunction } from "express";
 
 // Services
-import { registerUser } from "../../domain/authService";
+import { loginUser, registerUser } from "../../domain/authService";
 
 // Type definitions
 import { User as UserType } from "../../../../models/types";
 
 const router = express.Router();
 
-router.post("/register", async (req: Request, res: Response, next: NextFunction) => {
-    const { name, email, phone_number, password } = req.body;
+router.post(
+	"/register",
+	async (req: Request, res: Response, next: NextFunction) => {
+		const { name, email, phone_number, password } = req.body;
 
-    try {
-        const user: UserType = await registerUser(name, email, phone_number, password);
+		try {
+			const user: UserType = await registerUser(
+				name,
+				email,
+				phone_number,
+				password
+			);
 
-        res.status(201).json(user);
-    } catch (error) {
-        console.error("Error saving user:", error);
-        next(error);
-    }
-});
+			res.status(201).json(user);
+		} catch (error) {
+			console.error("Registration error:", error);
+			next(error);
+		}
+	}
+);
+
+router.post(
+	"/login",
+	async (req: Request, res: Response, next: NextFunction) => {
+		const { email, password } = req.body;
+
+		try {
+			const result = await loginUser(email, password);
+            
+			res.status(200).json(result);
+		} catch (error) {
+			console.error("Login error:", error);
+			next(error);
+		}
+	}
+);
 
 export default router;
